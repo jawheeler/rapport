58fb40e24e20e566c021b2e4a9e47736fcaab240
Andrew Wheeler <andrew.wheeler@bookrenter.com>
2012-01-10 11:16:29 -0800

    Added report_generator_csv_test

---
+++ test/rapport/report_generators/report_generator_csv_test.rb
@@ -0,0 +1,56 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+require 'rapport/report_generators/report_generator_csv'
+
+class ReportTest < Test::Unit::TestCase
+  setup do
+    @rg_csv = Rapport::ReportGeneratorCsv.new
+  end
+  
+  context "cell_format Time" do
+    setup do
+      @time = Time.new
+    end
+    
+    execute do
+      @rg_csv.format(Time, @time)
+    end
+  
+    should "format as a string" do
+      assert_equal @time.strftime('%B %e, %Y'), @execute_result
+    end
+  end
+  
+  
+  context "cell_format Date" do
+    setup do
+      @date = Date.new
+    end
+    
+    execute do
+      @rg_csv.format(Date, @date)
+    end
+    
+    should "format the date as a time" do
+      assert_equal Time.utc(@date.year, @date.month, @date.day ).strftime('%B %e, %Y'), @execute_result
+    end
+  end
+  
+  context ".generate" do
+    setup do
+      @report = Rapport::TestReport.new(:format => 'csv', :to_string => true)
+    end
+    
+    execute do
+      @report.generate
+    end
+    
+    should "write the header" do
+      assert_match "Column 0,Column 1,Column 2,Column 3\n", @execute_result
+    end
+    
+    should "write rows" do
+      assert_match "1,T0 2,Formatted~2,~2~1\n", @execute_result
+      assert_match "1,T1 3,Formatted~3,~3~1\n", @execute_result
+    end
+  end
+end
